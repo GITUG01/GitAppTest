28d80e153c7fb9fb1521236ae8f0ceb06ce0a4c5
Alexandr <sasha-03-30@mail.ru>
2022-05-15 14:11:20 +0300

    Initial commit

---
+++ app/src/main/java/com/gitug01/gitapptest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gitug01.gitapptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
